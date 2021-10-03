765b788971f56f3b39bc955c786d8df51087b372
sael1988 <eranga.singappuli@gmail.com>
2021-10-03 21:07:46 +0800

    terraform scripts added

---
+++ Terraform/variables.auto.tfvars
@@ -0,0 +1,10 @@
+gcp_credentials = "bluesky-project.json"
+gcp_project_id = "bluesky-gke"
+gcp_region = "us-west1"
+gke_zones = ["us-west1-a"]
+gke_regional = false
+gke_network = "default"
+gke_subnetwork = "default"
+gke_cluster_name = "bluesky-cluster"
+gke_default_nodepool_name = "bluesky-node-pool"
+gke_service_account_name = "bluesky-sa@bluesky-gke.iam.gserviceaccount.com"
---
+++ Terraform/variables.tf
@@ -0,0 +1,48 @@
+variable "gcp_credentials"{
+    type = string
+    description = "Location of service account for GCP"
+}
+
+variable "gcp_project_id"{
+    type = string
+    description = "gcp project id"
+}
+
+variable "gcp_region"{
+    type = string
+    description = "gcp region"
+}
+
+variable "gke_cluster_name"{
+    type = string
+    description = "gcp cluster name"
+}
+
+variable "gke_zones"{
+    type = list(string)
+    description = "list of zones for the GKE cluster"
+}
+
+variable "gke_network"{
+    type = string
+    description = "VPC name"
+}
+
+variable "gke_subnetwork"{
+    type = string
+    description = "VPC subnetwork name"
+}
+
+variable "gke_default_nodepool_name"{
+    type = string
+    description = "gke default nodepool name"
+}
+
+variable "gke_service_account_name"{
+    type = string
+    description = "gke service account name"
+}
+
+variable "gke_regional"{
+    description = "gke regional true/false"
+}
