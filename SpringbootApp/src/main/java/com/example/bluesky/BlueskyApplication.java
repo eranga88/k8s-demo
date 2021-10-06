ce6c61d912b3368d09af8264516ea9ef6d3e4e4b
sael1988 <eranga.singappuli@gmail.com>
2021-10-07 01:32:54 +0800

    test workflow

--- SpringbootApp/src/main/java/com/example/bluesky/BlueskyApplication.java
+++ SpringbootApp/src/main/java/com/example/bluesky/BlueskyApplication.java
@@ -24,7 +24,7 @@ public class BlueskyApplication {
 		ip = InetAddress.getLocalHost();
 		hostname = ip.getHostName();
 
-		return ("Hello Kubenetes 789  ! from " + ip);
+		return ("Hello Kubenetes ! from " + ip);
 
 	}
 
