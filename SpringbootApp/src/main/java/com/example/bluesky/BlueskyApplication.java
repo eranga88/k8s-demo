5516500d64b3093ae675ac60b3e51a4bcf65fec0
sael1988 <eranga.singappuli@gmail.com>
2021-10-06 13:05:01 +0800

    blue green deployment testing

--- SpringbootApp/src/main/java/com/example/bluesky/BlueskyApplication.java
+++ SpringbootApp/src/main/java/com/example/bluesky/BlueskyApplication.java
@@ -24,7 +24,7 @@ public class BlueskyApplication {
 		ip = InetAddress.getLocalHost();
 		hostname = ip.getHostName();
 
-		return ("Hello Kubenetes eranga ! from " + ip);
+		return ("Hello Kubenetes ! from " + ip);
 
 	}
 
